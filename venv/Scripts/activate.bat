0ab4c9f9bb3e2700a0703708e6a560c62f38da49
TAAFEBR6 <bruno.ferreirasagomes@swisscom.com>
2021-03-28 16:59:40 +0200

    removes other things except getting started

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\TAAFEBR6\PycharmProjects\m153\venv
+set VIRTUAL_ENV=C:\school_projects\m153\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
